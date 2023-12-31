0e56a0fabd624d11e1c7c447ae28240a6e8f83f2
Phat Tran <ptran@strongtie.com>
2023-12-31 16:55:20 +0700

    Finish

--- SwordDamageCalculator/Program.cs
+++ SwordDamageCalculator/Program.cs
@@ -6,7 +6,26 @@ namespace SwordDamageCalculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var damage = new SwordDamage();
+            var random = new Random();
+
+            while (true)
+            {
+                Console.Write("0 for no magic/flaming, 1 for magic, 2 for flaming, 3 for both, anything else to quit: ");
+                var keyPressed = Console.ReadKey(false).KeyChar;                
+
+                if (keyPressed != '0' && keyPressed != '1' && keyPressed != '2' && keyPressed != '3') return;
+
+                for (int i = 0; i < 3; i++)
+                {
+                    damage.Roll += random.Next(1, 7);
+                }
+
+                damage.SetMagic(keyPressed == '1' || keyPressed == '3');
+                damage.SetFlaming(keyPressed == '2' || keyPressed == '3');
+
+                Console.WriteLine($"\nRolled {damage.Roll} for {damage.Damage} HP\n");
+            }            
         }
     }
 }
