da0d582dd43478ee974675f61c4dff2c4a27e1c6
Phat Tran <ptran@strongtie.com>
2023-12-31 15:46:48 +0700

    Add project files.

---
+++ SwordDamageCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SwordDamageCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
